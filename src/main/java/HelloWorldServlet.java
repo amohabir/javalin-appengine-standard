c7cde1bcbf97f67ab91cd41f86d58d7d919b2245
Arvind Mohabir <Arvind@kramphub.com>
2018-11-16 11:24:50 +0100

    Initial commit

---
+++ src/main/java/HelloWorldServlet.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import io.javalin.EmbeddedJavalin;
+import io.javalin.core.JavalinServlet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = {"/*"}, name = "MyServlet")
+public class HelloWorldServlet extends HttpServlet {
+  private static final Logger logger = LoggerFactory.getLogger(HelloWorldServlet.class);
+  private final JavalinServlet javalinServlet;
+
+  public HelloWorldServlet() {
+    javalinServlet = new EmbeddedJavalin()
+      .get("/", ctx -> ctx.result("Hello from Javalin running on GAE Standard Java8 runtime."))
+      .get("/hello/:name",
+        ctx -> {
+          final String name = ctx.pathParam("name");
+          logger.info("Name: {}", name);
+          ctx.result("Javalin running on GAE Java8 says: Hello: " + name);
+        })
+      .createServlet();
+  }
+
+  @Override
+  public void service(HttpServletRequest req, HttpServletResponse resp) {
+    javalinServlet.service(req, resp);
+  }
+}
